7ff4931a3cc98362444e014640801d7c22c25d47
Diogo Cabral <diogo.cabral.dev@gmail.com>
2020-01-13 15:31:41 -0300

    Renaming repository implementation.

--- app/src/main/java/dev/diogocabral/webookmark/repository/BookLocalRepository.kt
+++ app/src/main/java/dev/diogocabral/webookmark/repository/BookLocalRepository.kt
@@ -3,7 +3,7 @@ package dev.diogocabral.webookmark.repository
 import androidx.lifecycle.LiveData
 import dev.diogocabral.webookmark.model.Book
 
-class BookLocalRepositoryImpl(private val bookDAO: BookDAO) : BookRepository {
+class BookLocalRepository(private val bookDAO: BookDAO) : BookRepository {
 
     override suspend fun insert(book: Book) {
         bookDAO.insert(book)
