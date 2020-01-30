ed36ea3fda0ac1ad251900e01d76aad8c80c4a5f
Diogo Cabral <drcabral@thoughtworks.com>
2020-01-30 15:03:43 -0300

    Changing basic activity style.

--- app/src/main/java/dev/diogocabral/webookmark/ui/MainActivity.kt
+++ app/src/main/java/dev/diogocabral/webookmark/ui/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
         observeAllBooksList()
         observeGetBooksByTitle()
@@ -31,18 +30,6 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
         }
     }
 
