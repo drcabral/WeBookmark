d7004c4c2589ab882daabd16c1639c7e45dc6733
Diogo Cabral <drcabral@thoughtworks.com>
2020-01-31 11:18:04 -0300

    Adding splash screen.

---
+++ app/src/main/java/dev/diogocabral/webookmark/SplashScreenActivity.kt
@@ -0,0 +1,27 @@
+package dev.diogocabral.webookmark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.content.Intent
+import dev.diogocabral.webookmark.ui.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        goToHomeActivity()
+    }
+
+    private fun goToHomeActivity() {
+        val handle = Handler()
+
+        handle.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
